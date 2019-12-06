a8b385f2d041d38d398a4359b7b506f4b0a70c6e
ngocphat-fsdev <44118190+ngocphat-fsdev@users.noreply.github.com>
2019-12-06 22:34:01 +0700

    Adding Agency.sql to insert data of agency

---
+++ Agency.sql
@@ -0,0 +1,61 @@
+USE WebDatabase;
+
+INSERT INTO Agency (Location, Address, Phone, Email, Website, WorkTime)
+VALUES (
+    'Cafo TP. HCM',
+    'Lầu 8, Jabes Building, 244 Cống Quỳnh, Q.1, TP.HCM',
+    '1900 6510',
+    'info@cafo.vn',
+    'www.cafo.vn',
+    '9:00 AM - 6:00 PM từ Thứ 2 - Thứ 6'
+);
+
+INSERT INTO Agency (Location, Address, Phone, Email, Website, WorkTime)
+VALUES (
+    'Cafo Đà Nẵng',
+    'Tầng 9, Tòa nhà VietNam Post Đà Nẵng, 155 Nguyễn Văn Linh, P. Vĩnh Trung, Q. Thanh Khê, Đà Nẵng',
+    '(0236) 3 651968',
+    'info@cafo.vn',
+    'www.cafo.vn',
+    '9:00 AM - 6:00 PM từ Thứ 2 - Thứ 6'
+);
+
+INSERT INTO Agency (Location, Address, Phone, Email, Website, WorkTime)
+VALUES (
+    'Cafo Huế',
+    'Tầng 7, tòa nhà HCC, 28 Lý Thường Kiệt, TP. Huế',
+    '(0234) 6.526.868',
+    'info@cafo.vn',
+    'www.cafo.vn',
+    '9:00 AM - 6:00 PM từ Thứ 2 - Thứ 6'
+);
+
+INSERT INTO Agency (Location, Address, Phone, Email, Website, WorkTime)
+VALUES (
+    'Cafo Hải Phòng',
+    'Tầng 6, 152 Hoàng Văn Thụ, Hồng Bàng, Hải Phòng',
+    '(0225) 6 584 999',
+    'info@cafo.vn',
+    'www.cafo.vn',
+    '9:00 AM - 6:00 PM từ Thứ 2 - Thứ 6'
+);
+
+INSERT INTO Agency (Location, Address, Phone, Email, Website, WorkTime)
+VALUES (
+    'Cafo Hà Nội',
+    'Tầng 3, Tòa nhà 101 Láng Hạ, 101 Láng Hạ, Đống Đa, Hà Nội',
+    '(024) 3201 1228',
+    'info@cafo.vn',
+    'www.cafo.vn',
+    '9:00 AM - 6:00 PM từ Thứ 2 - Thứ 6'
+);
+
+INSERT INTO Agency (Location, Address, Phone, Email, Website, WorkTime)
+VALUES (
+    'Cafo Cần Thơ',
+    '183 Võ Văn Kiệt, An Thới, Bình Thủy, Cần Thơ',
+    '(0292) 3739 500',
+    'info@cafo.vn',
+    'www.cafo.vn',
+    '9:00 AM - 6:00 PM từ Thứ 2 - Thứ 6'
+);
