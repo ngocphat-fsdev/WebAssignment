eeb0bd4122b7cf3dedfd4b49d2a947edf48a11b1
Tin <maihuynhhuutin@gmail.com>
2019-12-06 12:17:55 +0700

    Edit model blog, restaurant

---
+++ Models/Blog.php
@@ -0,0 +1,51 @@
+<?php
+class Blog
+{
+    #object Models Blog (BlogID, PicLink, Content, Comment, Like, Dislike)
+    private $BlogID;
+    private $PicLink;
+    private $Content;
+    private $Comment;
+    private $LikeCount;
+    private $DislikeCount;
+
+    public function __construct($BlogID, $PicLink, $Content, $Comment, $LikeCount, $DislikeCount)
+    {
+        $this->BlogID = $BlogID;
+        $this->PicLink = $PicLink;
+        $this->Content = $Content;
+        $this->Comment = $Comment;
+        $this->Like = $LikeCount;
+        $this->Dislike = $DislikeCount;
+    }
+
+    public function getBlogID()
+    {
+        return $this->BlogID;
+    }
+
+    public function getPicLink()
+    {
+        return $this->PicLink;
+    }
+
+    public function getContent()
+    {
+        return $this->Content;
+    }
+
+    public function getComment()
+    {
+        return $this->Comment;
+    }
+
+    public function getLikeCount()
+    {
+        return $this->LikeCount;
+    }
+
+    public function getDislikeCount()
+    {
+        return $this->DislikeCount;
+    }
+}
---
+++ Models/Restaurant.php
@@ -0,0 +1,44 @@
+<?php
+class Restaurant
+{
+    #object Models Restaurant (ResID, Address, Name, Rating, WorkTime, Dislike)
+    private $ResID;
+    private $Address;
+    private $Name;
+    private $Rating;
+    private $WorkTime;
+
+    public function __construct($ResID, $Address, $Name, $Rating, $WorkTime)
+    {
+        $this->ResID = $ResID;
+        $this->Address = $Address;
+        $this->Name = $Name;
+        $this->Rating = $Rating;
+        $this->WorkTime = $WorkTime;
+    }
+
+    public function getResID()
+    {
+        return $this->ResID;
+    }
+
+    public function getAddress()
+    {
+        return $this->Address;
+    }
+
+    public function getName()
+    {
+        return $this->Name;
+    }
+
+    public function getRating()
+    {
+        return $this->Rating;
+    }
+
+    public function getWorkTime()
+    {
+        return $this->WorkTime;
+    }
+}
