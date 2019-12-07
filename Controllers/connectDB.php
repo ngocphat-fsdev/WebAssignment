37504dd89855ab5b81195729975d4854cae91ce4
ngocphat-fsdev <44118190+ngocphat-fsdev@users.noreply.github.com>
2019-12-07 15:10:48 +0700

    add disconnectDB.php

--- Controllers/connectDB.php
+++ Controllers/connectDB.php
@@ -5,4 +5,10 @@
     
     // Create connection
     $conn = new mysqli($servername, $username, $password, "webdatabase");
+    mysqli_set_charset($conn, 'UTF8');
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 ?>
