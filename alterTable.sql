562700bb36432d7fbf57e28bd8c27da08c1fb6c7
NguyenThai <tvdevice7@gmail.com>
2019-12-15 14:04:19 +0700

    clean Project

--- alterTable.sql
+++
@@ -1,5 +0,0 @@
-ALTER DATABASE webdatabase CHARACTER SET utf8 COLLATE utf8_vietnamese_ci;
-
-ALTER TABLE reviews DROP COLUMN ReviewPlaceName;
-ALTER TABLE reviews DROP COLUMN AddressOfReviewPlace;
-
